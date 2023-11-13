97e1fc022c46865db523515d7bd302d116f6c73e
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2023-11-12 21:01:53 -0600

    ultimos cambios

--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb-client');
-const cors = require('cors')
 const app = express();
 
-app.use(cors());
 const PORT = 3000;
 
 // Conexión a MongoDB
@@ -57,6 +55,35 @@ app.get('/equipos', async (req, res) => {
     res.status(200).json(equipos);
   } catch (error) {
     res.status(500).json({ error: 'Error al obtener los equipos' });
+  }
+});
+
+
+// Ruta para obtener todos los equipos (GET)
+app.get('/equipos', async (req, res) => {
+  try {
+    const equipos = await Equipo.find();
+    res.status(200).json(equipos);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los equipos' });
+  }
+});
+
+
+// Ruta para eliminar un equipos (DELETE)
+app.delete('/equipos/:id', async (req, res) => {
+  try {
+    const objectId = new ObjectId(req.params.id); // Convert id parameter to ObjectId
+    // Criterio de búsqueda (por ejemplo, eliminar el documento con el campo "nombre" igual a "Juan")
+    const result = await Equipo.deleteOne({ _id: objectId });
+
+    if (result.deletedCount === 1) {
+      res.status(200).json({ message: 'Document deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Document not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error al eliminar el equipo' });
   }
 });
 
--- src/football/equipos.component.js
+++ src/football/equipos.component.js
@@ -1,77 +1,73 @@
-import React from 'react';
-import { View, Text, StyleSheet , Button} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { Table, Row } from 'react-native-table-component';
+import axios from 'axios';
 
 export const HomeEquipos = ({ navigation: { goBack, navigate } }) => {
-  // Datos de ejemplo para los equipos y sus integrantes
-  const datosEquipos = [
-    {
-      nombreEquipo: 'Equipo A',
-      facultad: 'Facultad 1',
-      añoCiclo: '2023 - Ciclo 1',
-      torneo: 'Masculino',
-      integrantes: [
-        {
-          carnet: '20230001',
-          nombresApellidos: 'Juan Pérez',
-          fechaNacimiento: '1995-05-15',
-          genero: 'Masculino',
-          posicion: 'Portero',
-          numeroCamisa: '1',
-        },
-        // Otros integrantes...
-      ],
-    },
-    // Otros equipos...
-  ];
+    const [datosEquipos, setDatosEquipos] = useState(null);
 
-  // Cabeceras de la tabla
-  const headers = [
-    'Equipo',
-    'Facultad',
-    'Año y Ciclo',
-    'Torneo',
-    'Integrantes',
-  ];
+    useEffect(() => {
+        getAll()
+    }, [])
 
-  // Datos para la tabla
-  const tableData = datosEquipos.map((equipo) => [
-    equipo.nombreEquipo,
-    equipo.facultad,
-    equipo.añoCiclo,
-    equipo.torneo,
-    equipo.integrantes.length.toString(), // Mostrar la cantidad de integrantes
-  ]);
+    const getAll = async () => {
+        await axios.get('http://10.0.2.2:3000/equipos')
+            .then(response => {
+                console.log('Response:', response.data);
+                setDatosEquipos(response.data)
+            })
+            .catch(error => {
+                console.error('Error:', error.message);
+            });
+    };
 
-  return (
-    <View style={styles.container}>
-        <View>
+    // Cabeceras de la tabla
+    const headers = [
+        'Equipo',
+        'Facultad',
+        'Año y Ciclo',
+        'Torneo',
+        'Integrantes',
+    ];
+
+    // Datos para la tabla
+    const tableData = datosEquipos != null ? datosEquipos.map((equipo) => [
+        equipo.nombreEquipo,
+        equipo.facultad,
+        equipo.anioCiclo,
+        equipo.torneo,
+        equipo.jugadores.length // Mostrar la cantidad de integrantes
+    ]) : [];
+
+    return (
+        <View style={styles.container}>
+            <View>
                 <Button onPress={() => goBack()} title="Regresar" />
             </View>
-      <Text style={styles.titulo}>Inscripciones al Torneo</Text>
-      <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
-        <Row data={headers} style={styles.head} textStyle={styles.textoHeader} />
-      </Table>
-      <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
-        {tableData.map((rowData, index) => (
-          <Row
-            key={index}
-            data={rowData}
-            style={[styles.row, index % 2 && { backgroundColor: '#F7F6E7' }]}
-            textStyle={styles.textoFila}
-          />
-        ))}
-      </Table>
-    </View>
-  );
+            <Text style={styles.titulo}>Inscripciones al Torneo</Text>
+            <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+                <Row data={headers} style={styles.head} textStyle={styles.textoHeader} />
+            </Table>
+            <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+                {tableData.map((rowData, index) => (
+                    <Row
+                        key={index}
+                        data={rowData}
+                        style={[styles.row, index % 2 && { backgroundColor: '#F7F6E7' }]}
+                        textStyle={styles.textoFila}
+                    />
+                ))}
+            </Table>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  titulo: { fontSize: 20, marginBottom: 10, fontWeight: 'bold' },
-  head: { height: 50, backgroundColor: '#f1f8ff' },
-  row: { height: 40, backgroundColor: '#E7E6E1' },
-  textoHeader: { textAlign: 'center', fontWeight: 'bold' },
-  textoFila: { textAlign: 'center' },
+    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+    titulo: { fontSize: 20, marginBottom: 10, fontWeight: 'bold' },
+    head: { height: 50, backgroundColor: '#f1f8ff' },
+    row: { height: 40, backgroundColor: '#E7E6E1' },
+    textoHeader: { textAlign: 'center', fontWeight: 'bold' },
+    textoFila: { textAlign: 'center' },
 });
 
