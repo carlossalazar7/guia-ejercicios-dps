4ab9a71051f74c7f10f751c450045464a9ac6f0e
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2023-11-12 18:54:38 -0600

    registro equipos

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb-client');
+
+const app = express();
+const PORT = 3000;
+
+// Conexión a MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/torneoDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  wtimeout: 60000, // Set timeout to 30 seconds
+});
+
+// Definir el esquema del equipo
+const equipoSchema = new mongoose.Schema({
+  nombreEquipo: String,
+  facultad: String,
+  anioCiclo: String,
+  torneo: String,
+  jugadores: [
+    {
+      carnet: String,
+      nombres: String,
+      apellidos: String,
+      fechaNacimiento: String,
+      genero: String,
+      posicion: String,
+      numeroCamisa: String,
+    },
+  ],
+});
+
+const Equipo = mongoose.model('Equipo', equipoSchema);
+
+app.use(bodyParser.json());
+
+// Ruta para registrar un equipo (POST)
+app.post('/equipos', async (req, res) => {
+  try {
+    const equipo = new Equipo(req.body);
+    await equipo.save();
+    res.status(201).json({ mensaje: 'Equipo registrado con éxito' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Error al registrar el equipo' });
+  }
+});
+
+// Ruta para obtener todos los equipos (GET)
+app.get('/equipos', async (req, res) => {
+  try {
+    const equipos = await Equipo.find();
+    res.status(200).json(equipos);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los equipos' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
--- src/football/home.component.js
+++ src/football/home.component.js
@@ -2,6 +2,20 @@ import React, { useState } from 'react';
 import { View, TextInput, Button, ScrollView, Text } from 'react-native';
 import { styles } from './styles';
 
+import axios from 'axios';
+
+const guardarEquipoEnMongoDB = async (equipo) => {
+    try {
+        const response = await axios.post('http://localhost:3000/equipos', equipo);
+        console.log(response.data); // Mensaje del servidor
+    } catch (error) {
+        console.error('Error al guardar el equipo en MongoDB:', error);
+    }
+};
+
+
+
+
 
 
 const RegistroEquipos = () => {
@@ -30,6 +44,19 @@ const RegistroEquipos = () => {
         // Agregar lógica para enviar los datos del equipo y jugadores a la base de datos o realizar acciones necesarias
         console.log('Datos del equipo:', { nombreEquipo, facultad, anioCiclo, torneo });
         console.log('Jugadores:', jugadores);
+
+        // Ejemplo de uso:
+        const nuevoEquipo = {
+            nombreEquipo: nombreEquipo,
+            facultad: facultad,
+            anioCiclo: anioCiclo,
+            torneo: torneo,
+            jugadores: jugadores
+        };
+
+        guardarEquipoEnMongoDB(nuevoEquipo);
+
+
     };
 
     return (
@@ -68,7 +95,48 @@ const RegistroEquipos = () => {
                     <View key={index}>
                         {/* Componente para registrar datos del jugador */}
                         <Text>Jugador {index + 1}</Text>
-                        {/* Agregar aquí campos para carnet, nombres, apellidos, fechaNacimiento, genero, posición, número de camisa */}
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Carnet"
+                            value={jugador.carnet}
+                            onChangeText={(text) => setJugadores({ ...nuevoJugador, carnet: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Nombres"
+                            value={jugador.nombres}
+                            onChangeText={(text) => setJugadores({ ...nuevoJugador, nombres: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Apellidos"
+                            value={jugador.apellidos}
+                            onChangeText={(text) => setJugadores({ ...nuevoJugador, apellidos: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Fecha Nacimiento"
+                            value={jugador.fechaNacimiento}
+                            onChangeText={(text) => setJugadores({ ...nuevoJugador, fechaNacimiento: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Genero"
+                            value={jugador.genero}
+                            onChangeText={(text) => setJugadores({ ...nuevoJugador, genero: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Posicion"
+                            value={jugador.posicion}
+                            onChangeText={(text) => setJugadores({ ...nuevoJugador, posicion: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Numero camisa"
+                            value={jugador.numeroCamisa}
+                            onChangeText={(text) => setJugadores({ ...nuevoJugador, numeroCamisa: text })}
+                        />
                     </View>
                 ))}
                 <View style={styles.botones}>
